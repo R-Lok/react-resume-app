70c261ee7299ac849a69bcc0257b31833773f703
Ryan Lok <rlok.pc@gmail.com>
2025-07-30 23:15:50 -0700

    Add styling to input elements

--- src/components/NamedInput.jsx
+++ src/components/NamedInput.jsx
@@ -1,8 +1,16 @@
 export default function NamedInput({ id, fieldName, currentValue, setter, inputType }) {
 
     const labelStyle = {
-        'display': 'block'
+        'display': 'block',
+        'font-weight': 'bold'
     }
+
+    const inputBorder = {
+        'border': '1px solid white',
+        'border-radius': '10px',
+        'padding': '5px' 
+    }
+    
 
 
     return (
@@ -10,11 +18,12 @@ export default function NamedInput({ id, fieldName, currentValue, setter, inputT
             <label htmlFor={id} style={labelStyle}>{fieldName}</label>
             {
                 inputType === 'textarea' ? 
-                    (<textarea id={id} value={currentValue} onChange={e => setter(e.target.value)}>
+                    (<textarea style={inputBorder} id={id} value={currentValue} onChange={e => setter(e.target.value)}>
 
                     </textarea>)
                 :
                     (<input
+                        style={inputBorder}
                         id={id}
                         type={inputType}
                         value={currentValue}
